3fcbb9544bfe650cd84ed989181ce00cde64ac64
07w <gimenezdanielv@gmail.com>
2023-02-25 16:20:04 +0100

    Cambios

--- app/src/main/java/com/example/grupalpokemon/Vistas/MainActivity.java
+++ app/src/main/java/com/example/grupalpokemon/Vistas/MainActivity.java
@@ -1,16 +1,75 @@
-package com.example.grupalpokemon.Vistas;
-
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.os.Handler;
+import android.os.Looper;
 
+import com.example.grupalpokemon.BBDD.Equipos_ADO;
+import com.example.grupalpokemon.BBDD.Pokemon_ADO;
+import com.example.grupalpokemon.Json.JsonPokemon;
+import com.example.grupalpokemon.Json.JsonHabilidades;
+import com.example.grupalpokemon.Json.JsonMovimientos;
+import com.example.grupalpokemon.Modelos.EquipoModelo;
+import com.example.grupalpokemon.Modelos.Pokemon;
 import com.example.grupalpokemon.R;
+import com.example.grupalpokemon.Vistas.Login;
 
-public class MainActivity extends AppCompatActivity {
+import java.util.ArrayList;
+
+public class MainActivity extends AppCompatActivity implements JsonPokemon.DownloadJSONCallback, JsonMovimientos.DownloadJSONCallback, JsonHabilidades.DownloadJSONCallback{
+    public static ArrayList<Pokemon> pokemonlist;
+    public static ArrayList<Pokemon> listafiltrada;
+    //  public static ArrayList<EquipoModelo> listadeequipos;
+
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        listafiltrada = new ArrayList<>();
+        JsonPokemon downloadJson = new JsonPokemon(this);
+        JsonMovimientos downloadJsonDos = new JsonMovimientos(this);
+        JsonHabilidades downloadabilities = new JsonHabilidades(this);
+
+        downloadJsonDos.execute("https://raw.githubusercontent.com/fanzeyi/pokemon.json/master/moves.json");
+        downloadabilities.execute("https://raw.githubusercontent.com/diurvi15/TrabajoGrupalBlackjack/main/soloabilitiesarray.json");
+        downloadJson.execute("https://raw.githubusercontent.com/diurvi15/TrabajoGrupalBlackjack/main/pokedexconshinys.json");
+
     }
-}
+
+    @Override
+    public void onDownloadJSONComplete(String result) {
+        Handler handler = new Handler(Looper.myLooper());
+        handler.postDelayed(()-> {
+
+            Pokemon_ADO pado = new Pokemon_ADO(this);
+            //  pado.insertAll(); //PA CUANDO SE JODA LA BBDD
+
+            cargarbasesdatos(pado);
+
+            if(pokemonlist.size()==0){
+                pado.insertAll();
+                cargarbasesdatos(pado);
+            }
+
+            Intent intent = new Intent(getApplicationContext(), Login.class);
+            startActivity(intent);
+        }, 500);
+
+    }
+    private void cargarbasesdatos(Pokemon_ADO pado){
+
+        pokemonlist = (ArrayList<Pokemon>) pado.getAll();
+    }
+
+    @Override
+    public void onDownloadJSONAbilityComplete(String result) {
+
+    }
+
+    @Override
+    public void onDownloadJSONDosComplete(String result) {
+
+    }
--- app/src/main/java/com/example/grupalpokemon/Vistas/Pantalla_principal.java
+++ app/src/main/java/com/example/grupalpokemon/Vistas/Pantalla_principal.java
@@ -1,14 +1,91 @@
-package com.example.grupalpokemon.Vistas;
+import static com.example.grupalpokemon.Controladores.Comunes.mensaje;
+import static com.example.grupalpokemon.Controladores.Controlador_Equipo.arraystringlocal;
+import static com.example.grupalpokemon.Controladores.Controlador_Equipo.comprobarequipocompleto;
+import static com.example.grupalpokemon.Controladores.Controlador_Equipo.crearequipolocalvacio;
+import static com.example.grupalpokemon.Controladores.Controlador_Equipo.rellenarequipolocal;
+
+import android.content.Intent;
+import android.net.Uri;
+import android.os.Bundle;
+import android.widget.ImageView;
 
 import androidx.appcompat.app.AppCompatActivity;
 
-import android.os.Bundle;
+import com.example.grupalpokemon.BBDD.Equipos_ADO;
+import com.example.grupalpokemon.Modelos.Pokemon;
+import com.example.grupalpokemon.R;
+import com.example.grupalpokemon.Sonidos.Sonidos;
+import com.example.grupalpokemon.Vistas.Creditos_Oak;
+import com.example.grupalpokemon.Vistas.Equipo;
+import com.example.grupalpokemon.Vistas.Lista_Equipos;
+import com.example.grupalpokemon.Vistas.Listado;
+
+import java.util.ArrayList;
+import java.util.List;
 
 public class Pantalla_principal extends AppCompatActivity {
+    public static ArrayList<Pokemon> equipolocal;
+    private List<String> equipolocalstrings;
+    Equipos_ADO eado;
+    public static ImageView btshow;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_pantalla_principal);
+        Sonidos.crearsonidofondo(this);
+        ImageView dummy = findViewById(R.id.dummy);
+        ImageView bt1= findViewById(R.id.btlistapokemon);
+        ImageView bt2= findViewById(R.id.btequipo);
+        ImageView bt4= findViewById(R.id.btlistaequipo);
+        ImageView bt3= findViewById(R.id.btnoak);
+        btshow= findViewById(R.id.btshow);
+
+        cargarequipolocal();
+
+        clics(bt1, Listado.class);
+        clics(bt3, Creditos_Oak.class);
+        clics(bt2, Equipo.class);
+
+        btshow.setOnClickListener(v -> {
+            Intent browserIntent = new Intent(Intent.ACTION_VIEW, Uri.parse("https://play.pokemonshowdown.com/teambuilder"));
+            startActivity(browserIntent);
+        });
+
+        bt4.setOnClickListener(v->{
+            Equipos_ADO eado = new Equipos_ADO(this);
+            ArrayList<String> equipos = (ArrayList<String>) eado.getEquiposOtros();
+            if( equipos.size()>0){
+                intento(Lista_Equipos.class);
+            }
+            else{
+                mensaje(getString(R.string.nohayequipo),this);
+            }});
+
+        comprobarequipocompleto(dummy,dummy,dummy,dummy,dummy,dummy,1);
+    }
+
+    private void cargarequipolocal(){
+
+        eado= new Equipos_ADO(this);
+        equipolocal = new ArrayList<>();
+        crearequipolocalvacio();
+        equipolocalstrings = arraystringlocal(eado);
+        rellenarequipolocal(equipolocalstrings);
+    }
+
+    private void clics(ImageView bt,Class cla){
+
+        bt.setOnClickListener(v->{
+
+            intento(cla);
+        });
+
+    }
+
+    private void intento(Class clase){
+
+        Intent intent = new Intent(this,clase);
+        startActivity(intent);
     }
 }
